7aeeb762fa2c6e21427c3f5561d6643148abc6b1
Sahil <dshiv867@gmail.com>
2023-12-31 00:28:17 +0530

    created sidebar

--- app/(root)/Layout.1.tsx
+++ app/(root)/Layout.1.tsx
@@ -1,20 +1,16 @@
-import LeftSideBar from '@/components/shared/LeftSideBar';
 import Navbar from '@/components/shared/Navbar/Navbar';
-import RightSideBar from '@/components/shared/RightSideBar';
 import React from 'react';
 
-const Layout = ({ children }: { children: React.ReactNode }) => (
+export const Layout = ({ children }: { children: React.ReactNode }) => (
   <main className="background-light850_dark100 relative">
     <Navbar />
     <div className="flex">
-      <LeftSideBar />
+      LeftSidebar
       <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
         <div className="mx-auto w-full max-w-5xl">{children}</div>
       </section>
-      <RightSideBar />
+      RightSideBar
     </div>
     Toast
   </main>
 );
-
-export default Layout;
---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import RenderTag from './RenderTag';
+
+const hotQuestions = [
+  { _id: 1, question: 'how to make a project with react?' },
+  { _id: 2, question: 'how to make a project with react?' },
+  { _id: 3, question: 'how to make a project with react?' },
+  { _id: 4, question: 'how to make a project with react?' },
+  { _id: 5, question: 'how to make a project with react?' },
+];
+
+const popularTags = [
+  {
+    _id: 1,
+    name: 'react',
+    totalQuestions: 5,
+  },
+  {
+    _id: 2,
+    name: 'nextjs',
+    totalQuestions: 5,
+  },
+  {
+    _id: 3,
+    name: 'nodejs',
+    totalQuestions: 5,
+  },
+  {
+    _id: 4,
+    name: 'javascript',
+    totalQuestions: 5,
+  },
+];
+const RightSideBar = () => {
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden w-[350px] custom-scrollbar max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions </h3>
+        <div className="mt-7 w-full flex flex-col gap-[30px]">
+          {hotQuestions.map((question) => (
+            <Link
+              href={`/question/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.question}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="arrow"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16 ">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default RightSideBar;
