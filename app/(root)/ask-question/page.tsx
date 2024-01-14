3d65d7b8a198581db548143e81b7bf9ba4cafd88
sahil <dshiv867@gmail.com>
2024-01-14 22:06:02 +0530

    created ask question page

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,13 @@
+import Question from '@/components/forms/Question';
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900 ">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+};
+export default Page;
--- app/(root)/ask-questions/page.tsx
+++
@@ -1,4 +0,0 @@
-const page = () => {
-  return <div>Ask-questions</div>;
-};
-export default page;
